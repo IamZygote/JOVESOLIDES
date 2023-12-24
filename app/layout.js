e8465f3b449a9462d2d6a5e0dea735c3de97bd6f
SeifAbdelsamea <seifibrahim1232@gmail.com>
2023-12-24 13:36:56 +0200

    Update layout.js
    by adding this line ( quality = 100)
    the logo quality has changed to 100 instead of default 70 or 80.

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Logo from './logo.png'
-import Image from 'next/image'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Logo from "./logo.png";
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'jovesolides',
-  description: 'Erasmus-Jovesolides Egypt Home Webpage',
-}
+  title: "jovesolides",
+  description: "Erasmus-Jovesolides Egypt Home Webpage",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -16,12 +16,13 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Image
           src={Logo}
-          alt= 'jovesolides logo'
-          width = {100}
-          placeholder = "blur"
+          alt="jovesolides logo"
+          width={100}
+          quality={100}
+          placeholder="blur"
         />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
