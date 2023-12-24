41d31d38b5a2dfd87ca567476100c7e2040cc493
Mohamed Sofy <58863853+IamZygote@users.noreply.github.com>
2023-12-24 02:35:46 +0200

    The Big Bang
    This how the world begin

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Logo from './logo.png'
+import Image from 'next/image'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'jovesolides',
+  description: 'Erasmus-Jovesolides Egypt Home Webpage',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Image
+          src={Logo}
+          alt= 'jovesolides logo'
+          width = {100}
+          placeholder = "blur"
+        />
+        {children}
+        </body>
+    </html>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <center><h1>Jovesolides</h1></center>
+      </div>
+    </main>
+  )
+}
